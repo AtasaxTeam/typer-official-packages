fc61d62379c997817695ad40214a8cb260c31a46
grosik <99998467+grszik@users.noreply.github.com>
2025-07-18 23:10:45 +0100

    Update wordle.js

--- javascript/wordle.js
+++ javascript/wordle.js
@@ -19,20 +19,38 @@ async (data, input, exit) => {
       rev()
     })})
   } else log("Got the words from cache")
-  log(words.join(';'))
+  log('Guess the word!')
   let word = words[Math.floor(Math.random()*words.length)]
+  log(word)
   async function guess(resv) {
     let g = await input()
-    resv()
-    return true
+    if(g.length != 5) {
+      log("The word has to be 5 letters long.")
+      return guess(resv)
+    } else if(!words.includes(g.toUpperCase())) {
+      log("The word is not in the list.")
+      return guess(resv)
+    } else {
+      let string = ""
+      for(let i = 0; i < 5; i++) {
+        let color = "gray"
+        if(g[i] == word[i]) color = "green"
+        else if(word.includes(g[i])) color = "yellow"
+        string += `<span class="${color}">${g[i]}</span>`
+      }
+      log(string)
+      resv()
+      return g.toUpperCase() == word
+    }
   }
   for(let i = 0; i<6; i++) {
     let stop = false
+    log(`Guess ${i}:`)
     await new Promise((rev, rex) => {
       if(guess(rev)) stop = true
     })
     if(stop) break
   }
-  log(`Answer:\n<h2>.${word}.</h2>`)
+  log(`Answer:\n<h2>${word}</h2>`)
   exit()
 }
