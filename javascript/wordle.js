1a5480dfd0daf567eeeda11b9fe07ddb1dbfeae0
grosik <99998467+grszik@users.noreply.github.com>
2025-07-18 19:38:05 +0100

    Update wordle.js

--- javascript/wordle.js
+++ javascript/wordle.js
@@ -2,7 +2,7 @@ async (data, input, exit) => {
   let words = command_data["wordle_words"]
   if(!words) {
     fetch("https://sabera.ovh/cdn/wordle.txt")
-    .then(r => {
+    .then(async (r) => {
       let res = await res.text()
       if(r.status != 200) {
         log('<span class="red">Something went wrong when getting the words</span>')
