a50280ace70eaa260495a2225687b3636d80aeff
grosik <99998467+grszik@users.noreply.github.com>
2025-07-18 22:03:15 +0100

    Update wordle.js

--- javascript/wordle.js
+++ javascript/wordle.js
@@ -6,19 +6,31 @@ async (data, input, exit) => {
     await new Promise((rev, rex) => {
       fetch("https://sabera.ovh/cdn/wordle.txt")
     .then(async (r) => {
-      let res = await r.text()
+      let res = await res.text()
       if(r.status != 200) {
         log('<span class="red">Something went wrong when getting the words</span>')
         exit()
         return
       }
-      words = res.split('\n')
+      words = res.split(' \n')
       command_data['wordle_words'] = words
 
       log("Got the words")
       rev()
     })})
   } else log("Got the words from cache")
-  log(words.join('|'))
+  log(words.join(', '))
+  let word = words[Math.floor(Math.random()*words.length)]
+  async function guess(resv) {
+    let g = await input()
+    resv()
+    return true
+  }
+  for(let i = 0; i<96; i++) {
+    await new Promise((rev, rex) => {
+      if(guess(rev)) break
+    })
+  }
+  log(`Answer:\n<h2>${word}</h2>`)
   exit()
 }
