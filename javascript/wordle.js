42124279d3482aaade66905caebcc1ac8d625d4c
grosik <99998467+grszik@users.noreply.github.com>
2025-07-18 19:44:44 +0100

    Update wordle.js

--- javascript/wordle.js
+++ javascript/wordle.js
@@ -1,6 +1,7 @@
 async (data, input, exit) => {
   let words = command_data["wordle_words"]
   if(!words) {
+    log('Fetching the words')
     fetch("https://sabera.ovh/cdn/wordle.txt")
     .then(async (r) => {
       let res = await res.text()
@@ -12,8 +13,9 @@ async (data, input, exit) => {
       words = res.split('\n')
       command_data['wordle_words'] = words
 
-      log('Got the words.')
+      log("Got the words")
     })
-  }
-  log(words.join(','))
+  } else log("Got the words from cache")
+  log(words.join(', '))
+  exit()
 }
